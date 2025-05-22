dde23a747f0f2c548eee03198bb5f53f8bc8dc0a
MaoSong2022 <maosong@pjlab.org.cn>
2025-05-22 10:52:55 +0800

    feat(bpe_tokenizer.py): optimize efficiency

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -2,91 +2,93 @@ import regex as re
 from collections import defaultdict
 
 
-class BPETokenizer:
-    def __init__(self, vocab_size: int, special_tokens: list[str]):
-        self.vocab_size = vocab_size
-        self.special_tokens = special_tokens
-        self.vocab = {}  # dict[int, bytes]
-        self.inverse_vocab = {}  # dict[bytes, int]
-        self.merges = []  # list[tuple[bytes, bytes]]
+def pre_tokenize(file_path: str) -> list[list[str]]:
+    with open(file_path) as f:
+        data = f.read()
 
-    def pre_tokenize(self, file_path: str) -> list[list[str]]:
-        with open(file_path) as f:
-            data = f.read()
+    chunks = re.split(r"<\|endoftext\|>", data)
+    pattern = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    pre_tokenized_chunks = [re.findall(pattern, chunk) for chunk in chunks]
+    return pre_tokenized_chunks
 
-        chunks = re.split(r"<\|endoftext\|>", data)
-        pattern = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-        pre_tokenized_chunks = [re.findall(pattern, chunk) for chunk in chunks]
-        return pre_tokenized_chunks
 
-    def get_stats(
-        self, bytes_tuple_counter: defaultdict[tuple[bytes, bytes], int]
-    ) -> defaultdict[tuple[bytes, bytes], int]:
-        counter = defaultdict(int)
-        for bytes_tuple, count in bytes_tuple_counter.items():
-            for i in range(len(bytes_tuple) - 1):
-                counter[(bytes_tuple[i], bytes_tuple[i + 1])] += count
+def get_stats(splits: dict[bytes, list[bytes]], word_freqs: dict[bytes, int]) -> defaultdict[tuple[bytes, bytes], int]:
+    pair_freqs = defaultdict(int)
+    for word, freq in word_freqs.items():
+        split = splits[word]
+        if len(split) == 1:
+            continue
 
-        return counter
+        for i in range(len(split) - 1):
+            pair_freqs[(split[i], split[i + 1])] += freq
 
-    def merge_pairs(
-        self, bytes_tuple_counter: defaultdict[tuple[bytes, bytes], int], merge_pair: tuple[bytes, bytes]
-    ) -> defaultdict[tuple[bytes, bytes], int]:
-        counter = defaultdict(int)
-        for bytes_tuple, count in bytes_tuple_counter.items():
-            bytes_list = []
-            i = 0
-            while i < len(bytes_tuple):
-                if i < len(bytes_tuple) - 1:
-                    pair = (bytes_tuple[i], bytes_tuple[i + 1])
-                    if pair == merge_pair:
-                        bytes_list.append(merge_pair[0] + merge_pair[1])
-                        i += 2
-                        continue
+    return pair_freqs
 
-                bytes_list.append(bytes_tuple[i])
+
+def get_merge_pair(pair_freqs: defaultdict[tuple[bytes, bytes], int]) -> tuple[tuple[bytes, bytes], int]:
+    best_pair = None
+    max_freq = -1
+    for pair, freq in pair_freqs.items():
+        if freq > max_freq or (freq == max_freq and pair > best_pair):
+            max_freq = freq
+            best_pair = pair
+
+    return best_pair, max_freq
+
+
+def merge_pairs(splits: dict[bytes, list[bytes]], merge_pair: tuple[bytes, bytes]) -> dict[bytes, list[bytes]]:
+    new_subword = merge_pair[0] + merge_pair[1]
+    for word in splits:
+        split = splits[word]
+        if len(split) == 1:
+            continue
+
+        if new_subword not in word:
+            continue
+
+        i = 0
+        while i < len(split) - 1:
+            if split[i] == merge_pair[0] and split[i + 1] == merge_pair[1]:
+                split = split[:i] + [new_subword] + split[i + 2 :]
+            else:
                 i += 1
-            counter[tuple(bytes_list)] += count
 
-        return counter
+        splits[word] = split
 
-    def train(self, file_path: str) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-        # add special tokens
-        self.vocab = {i: token.encode("utf-8") for i, token in enumerate(self.special_tokens)}
-        self.inverse_vocab = {token.encode("utf-8"): i for i, token in enumerate(self.special_tokens)}
+    return splits
 
-        # add initial 256 characters
-        for i in range(256):
-            self.vocab[len(self.inverse_vocab)] = bytes([i])
-            self.inverse_vocab[bytes([i])] = len(self.vocab)
 
-        pre_tokenized_chunks = self.pre_tokenize(file_path)
-        word_counts = defaultdict(int)
-        for chunk in pre_tokenized_chunks:
-            for word in chunk:
-                word_counts[word] += 1
+def train_bpe(
+    vocab_size: int, special_tokens: list[str], file_path: str
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    # Initialize vocabulary and merges
+    vocab = {}  # dict[int, str]
+    merges = []  # list[tuple[bytes, bytes]]
 
-        bytes_tuple_counter = defaultdict(int)
-        for word, count in word_counts.items():
-            bytes_tuple = tuple(bytes([c]) for c in word.encode("utf-8"))
-            bytes_tuple_counter[bytes_tuple] += count
+    # add special tokens
+    vocab = {i: token.encode("utf-8") for i, token in enumerate(special_tokens)}
 
-        while len(self.vocab) < self.vocab_size:
-            new_token_id = len(self.vocab)
-            stats = self.get_stats(bytes_tuple_counter)
-            # find the best merge pair
-            best_score = (0, bytes([0]), bytes([0]))
-            for pair, count in stats.items():
-                current_score = (count, pair[0], pair[1])
-                if current_score > best_score:
-                    best_score = current_score
+    # add initial 256 characters
+    for i in range(256):
+        vocab[len(vocab)] = bytes([i])
 
-            freq, merge_pair = best_score[0], (best_score[1], best_score[2])
+    pre_tokenized_chunks = pre_tokenize(file_path)
+    # print(pre_tokenized_chunks)
 
-            bytes_tuple_counter = self.merge_pairs(bytes_tuple_counter, merge_pair)
-            new_bytes = merge_pair[0] + merge_pair[1]
-            self.vocab[new_token_id] = new_bytes
-            self.inverse_vocab[new_bytes] = new_token_id
-            self.merges.append(merge_pair)
+    word_freqs = defaultdict(int)
+    for chunk in pre_tokenized_chunks:
+        for word in chunk:
+            word_freqs[word.encode("utf-8")] += 1
 
-        return self.vocab, self.merges
+    splits = {word: [bytes([i]) for i in word] for word in word_freqs}
+
+    while len(vocab) < vocab_size:
+        pair_freqs = get_stats(splits, word_freqs)
+        # find the best merge pair
+        merge_pair, freq = get_merge_pair(pair_freqs)
+        splits = merge_pairs(splits, merge_pair)
+        new_token_id = len(vocab)
+        vocab[new_token_id] = merge_pair[0] + merge_pair[1]
+        merges.append((merge_pair[0], merge_pair[1]))
+
+    return vocab, merges
