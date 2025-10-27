045954dbeb6f52ca97dcd5c00eedf9b95d2c82ba
MaoSong2022 <maosong@pjlab.org.cn>
2025-10-27 17:55:00 +0800

    feat(utils.py): add implementation of softmax

--- cs336_basics/model/utils.py
+++ cs336_basics/model/utils.py
@@ -7,4 +7,5 @@ def SiLU(x: torch.Tensor) -> torch.Tensor:
 
 def softmax(x: torch.Tensor, dim: int = -1) -> torch.Tensor:
     x = x - x.max(dim=dim, keepdim=True).values
-    return x
+    log_sum_exp = torch.log(torch.sum(torch.exp(x), dim=dim, keepdim=True))
+    return torch.exp(x - log_sum_exp)
