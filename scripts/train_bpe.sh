3a300a15de8dd39f9278d3583f2f25cba2fc28c1
MaoSong2022 <maosong@pjlab.org.cn>
2025-12-26 11:42:44 +0800

    feat(train_bpe.sh): add script to run train_bpe

---
+++ scripts/train_bpe.sh
@@ -0,0 +1,2 @@
+python cs336_basics/tokenizer/train_bpe.py -i data/TinyStoriesV2-GPT4-train.txt -v 10000 -o data/TinyStories
+python cs336_basics/tokenizer/train_bpe.py -i data/owt_train.txt -v 32000 -o data/OWT
