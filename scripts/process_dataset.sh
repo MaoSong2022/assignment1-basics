9cdef7d4e02e0512e8f4da19721d45a000c8ed23
MaoSong2022 <maosong@pjlab.org.cn>
2025-12-27 18:05:52 +0800

    feat(process_dataset.sh): add processing scripts

---
+++ scripts/process_dataset.sh
@@ -0,0 +1,4 @@
+python ./scripts/preprocess_dataset.py --tokenizer_path data/TinyStories/tokenizer.json --input_filepath data/TinyStoriesV2-GPT4-train.txt
+python ./scripts/preprocess_dataset.py --tokenizer_path data/TinyStories/tokenizer.json --input_filepath data/TinyStoriesV2-GPT4-valid.txt
+python ./scripts/preprocess_dataset.py --tokenizer_path data/OWT/tokenizer.json --input_filepath data/owt_train.txt
+python ./scripts/preprocess_dataset.py --tokenizer_path data/OWT/tokenizer.json --input_filepath data/owt_valid.txt
